3ff8c7596dd458c05904c26c3005e198822f1a00
Mete <burakmetecanpinar@gmail.com>
2025-12-28 23:44:41 +0300

    updated to spring 4 + java 25

--- src/test/java/com/ecommerce/payment/controller/PaymentControllerTest.java
+++ src/test/java/com/ecommerce/payment/controller/PaymentControllerTest.java
@@ -3,8 +3,8 @@ package com.ecommerce.payment.controller;
 import com.ecommerce.payment.service.PaymentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.ecommerce.payment.service.PaymentService.HELLO_FROM_PAYMENT_SERVICE;
@@ -20,7 +20,7 @@ class PaymentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private PaymentService paymentService;
 
     @Test
